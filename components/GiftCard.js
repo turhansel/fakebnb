9d3962e25c2d71dcd3ebe88a701f8e3584b201aa
turhansel <turhansel@gmail.com>
2021-12-19 22:43:15 +0300

    :sparkles: feat(ui): add giftcard component

---
+++ components/GiftCard.js
@@ -0,0 +1,46 @@
+import Image from 'next/image'
+import React from 'react'
+
+const GiftCard = () => {
+	return (
+		<div className='flex items-center justify-center '>
+			<div
+				className='w-[800px] h-[400px]  md:w-[1600px] md:h-[800px] bg-white rounded-xl  grid grid-cols-2 gap-4 place-content-center relative px-4 py-10shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-60 border border-gray-200 backdrop-filter-blur  '
+				// style={{ backdropFilter: 'blur(20px)' }}
+			>
+				<div className='flex items-center flex-col justify-center space-y-7 '>
+					<span className='text-2xl'>Introducing</span>
+					<h2 className='text-5xl font-semibold'>Airbnb gift cards</h2>
+					<button className='bg-black text-white py-2 px-4 shadow-md rounded-lg  font-medium active:scale-90 transition duration-150 '>
+						Shop Now
+					</button>
+				</div>
+				<div className='flex items-center relative'>
+					<div className='rotate-[15deg] z-20 mb-8'>
+						<Image
+							className='rounded-xl'
+							alt='brand-gift'
+							src='https://res.cloudinary.com/diuzwbnzu/image/upload/v1639923980/51CmtqNRN9L_zjvl4j.jpg'
+							width={430}
+							height={300}
+							objectFit='cover'
+						/>
+					</div>
+
+					<div className='absolute top-36 -rotate-[31deg] md:ml-64'>
+						<Image
+							className='rounded-xl '
+							alt='brand-gift'
+							src='https://res.cloudinary.com/diuzwbnzu/image/upload/v1639923984/Airbnb_Logo_Over_Gradient_kx38qc.png'
+							width={430}
+							height={300}
+							objectFit='cover'
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default GiftCard
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Banner from '../components/Banner'
 import Header from '../components/Header'
 import { getSession, useSession } from 'next-auth/react'
 import Login from '../components/Login'
+import Main from '../components/Main'
+import GiftCard from '../components/GiftCard'
 
 export const Home = ({}) => {
 	const { data: session } = useSession()
@@ -14,9 +16,14 @@ export const Home = ({}) => {
 					<title>Airbnb-ish</title>
 					<link rel='icon' href='/favicon.ico' />
 				</Head>
+
 				<Header page='/' />
-				<Banner />
-				{/* Main */}
+				<div className='bg-gradient-to-r from-indigo-300 via-pink-300 to-purple-300'>
+					<Banner />
+					<GiftCard />
+				</div>
+
+				{/* <Main /> */}
 				{/* Footer */}
 			</div>
 		)
