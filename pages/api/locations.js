b7cc8faa15c1f609e86be5cd3f5b9a0437d8ea26
Turhan Sel <turhansel@gmail.com>
2022-01-09 03:28:59 +0300

    :sparkles: feat: create data and api route for location info

---
+++ pages/api/locations.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { locations } from '../../data';
+
+export default function handler(req, res) {
+	res.status(200).json(locations);
+}
