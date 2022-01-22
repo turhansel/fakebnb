677dc01c89ed093d2b324dd475dd206854c208b7
Turhan Sel <turhansel@gmail.com>
2022-01-23 00:43:12 +0300

    :sparkles: feat(progressbar): create progressbar component with nextjs-progressbar and add to _app.js for for routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
+import ProsressBar from '../components/ProgressBar';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
 		<ThemeProvider enableSystem={false} attribute='class'>
 			<SessionProvider session={session}>
+				<ProsressBar />
 				<Component {...pageProps} />
 			</SessionProvider>
 		</ThemeProvider>
